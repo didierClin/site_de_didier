4274fab2c40fb763b8f004a8c8790deb36b30e01
didierClin <didier.c62@numericable.fr>
2016-03-10 23:30:42 +0100

    Added controller projects and administrate

---
+++ app/dashboards/project_dashboard.rb
@@ -0,0 +1,60 @@
+require "administrate/base_dashboard"
+
+class ProjectDashboard < Administrate::BaseDashboard
+  # ATTRIBUTE_TYPES
+  # a hash that describes the type of each of the model's fields.
+  #
+  # Each different type represents an Administrate::Field object,
+  # which determines how the attribute is displayed
+  # on pages throughout the dashboard.
+  ATTRIBUTE_TYPES = {
+    id: Field::Number,
+    title: Field::String,
+    description: Field::Text,
+    language: Field::String,
+    slug: Field::String,
+    created_at: Field::DateTime,
+    updated_at: Field::DateTime,
+  }
+
+  # COLLECTION_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's index page.
+  #
+  # By default, it's limited to four items to reduce clutter on index pages.
+  # Feel free to add, remove, or rearrange items.
+  COLLECTION_ATTRIBUTES = [
+    :id,
+    :title,
+    :description,
+    :language,
+  ]
+
+  # SHOW_PAGE_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's show page.
+  SHOW_PAGE_ATTRIBUTES = [
+    :id,
+    :title,
+    :description,
+    :language,
+    :slug,
+    :created_at,
+    :updated_at,
+  ]
+
+  # FORM_ATTRIBUTES
+  # an array of attributes that will be displayed
+  # on the model's form (`new` and `edit`) pages.
+  FORM_ATTRIBUTES = [
+    :title,
+    :description,
+    :language,
+    :slug,
+  ]
+
+  # Overwrite this method to customize how projects are displayed
+  # across all pages of the admin dashboard.
+  #
+  # def display_resource(project)
+  #   "Project ##{project.id}"
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
+
+  namespace :admin do
+    resources :projects
+
+    root to: "projects#index"
+  end
+
   root 'pages#index'
 
   get 'pages/about'
+
+  get 'projects' => 'projects#index'
+
+  get 'projects/show'
 
 end
