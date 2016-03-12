2797d55a9134e8433280bf6b09eb2f2667517d85
didierClin <didier.c62@numericable.fr>
2016-03-12 21:10:05 +0100

    Add style to static IndexPage and favicon

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,7 +3,7 @@
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
 // or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
+//d 16
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // compiled file.
 //
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,6 +4,7 @@ class ProjectsController < ApplicationController
   end
 
   def show
+    @project = Project.find(params[:project])
   end
 
 end
